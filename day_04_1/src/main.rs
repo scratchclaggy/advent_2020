fd16685f2ca11cc108cc8cfbf5b999ba86d96464
James van de Ven <james@jamesvandeven.com>
2020-12-06 15:45:04 +0900

    completed day_04_1

--- day_04_1/src/main.rs
+++ day_04_1/src/main.rs
@@ -1,41 +1,14 @@
 use std::collections::HashMap;
 use std::fs;
 
-const FILENAME: &str = "ex.txt";
+const FILENAME: &str = "input.txt";
 
 fn main() {
     let input = fs::read_to_string(FILENAME).expect("Error: could not open file");
 
-    let new_passport = |input_string: &str| {
-        let mut passport: HashMap<&str, &str> = HashMap::new();
-        for field in input_string.split(|c| c == ' ' || c == '\n') {
-            let kv_pair: Vec<&str> = field.split(':').collect();
-            passport.insert(kv_pair[0], kv_pair[1]);
-        }
-
-        passport
-    };
-
-    let is_okay = |passport: &HashMap<&str, &str>| {
-        let mut okay = false;
-        if passport.contains_key("byr")
-            && passport.contains_key("iyr")
-            && passport.contains_key("eyr")
-            && passport.contains_key("hgt")
-            && passport.contains_key("hcl")
-            && passport.contains_key("ecl")
-            && passport.contains_key("pid")
-        {
-            okay = true;
-        }
-
-        okay
-    };
-
     let answer: usize = input
         .split("\r\n\r\n")
-        .map(|s| new_passport(s))
-        .filter(|p| is_okay(p))
+        .filter(|s| Passport::new(s).is_okay())
         .count();
 
     println!("The answer is: {}\n", answer);
@@ -43,10 +16,31 @@ fn main() {
     println!("END OF Program\n");
 }
 
-// type Passport<'a> = HashMap<&'a str, &'a str>;
+struct Passport<'a> {
+    field: HashMap<&'a str, &'a str>,
+}
 
-// impl<'a> Passport<'a> {
-//     fn new(passport_string: &str) -> Passport {
-//         Passport {}
-//     }
-// }
+impl<'a> Passport<'a> {
+    fn new(passport_string: &'a str) -> Passport {
+        let mut field: HashMap<&str, &str> = HashMap::new();
+        for entry in passport_string.split(|c| c == ' ' || c == '\n') {
+            let kv_pair: Vec<&str> = entry.split(':').collect();
+            field.insert(kv_pair[0], kv_pair[1]);
+        }
+        Passport { field }
+    }
+
+    fn is_okay(&self) -> bool {
+        if self.field.contains_key("byr")
+            && self.field.contains_key("iyr")
+            && self.field.contains_key("eyr")
+            && self.field.contains_key("hgt")
+            && self.field.contains_key("hcl")
+            && self.field.contains_key("ecl")
+            && self.field.contains_key("pid")
+        {
+            return true;
+        }
+        false
+    }
+}
--- day_04_2/src/main.rs
+++ day_04_2/src/main.rs
@@ -1,3 +1,154 @@
+use std::collections::HashMap;
+use std::fs;
+
+const FILENAME: &str = "ex.txt";
+
 fn main() {
-    println!("Hello, world!");
+    let input = fs::read_to_string(FILENAME).expect("Error: could not open file");
+
+    let answer: usize = input
+        .split("\r\n\r\n")
+        .filter(|s| Passport::new(s).is_okay())
+        .count();
+
+    println!("The answer is: {}\n", answer);
+
+    println!("END OF Program\n");
+}
+
+struct Passport<'a> {
+    field: HashMap<&'a str, &'a str>,
+}
+
+impl<'a> Passport<'a> {
+    fn new(passport_string: &'a str) -> Passport {
+        let mut field: HashMap<&str, &str> = HashMap::new();
+        for entry in passport_string.split(|c| c == ' ' || c == '\n') {
+            let kv_pair: Vec<&str> = entry.split(':').collect();
+            field.insert(kv_pair[0], kv_pair[1]);
+        }
+        Passport { field }
+    }
+
+    fn is_okay(&self) -> bool {
+        println!("byr: {}", self.byr_okay());
+        println!("iyr: {}", self.iyr_okay());
+        println!("eyr: {}", self.eyr_okay());
+        println!("hgt: {}", self.hgt_okay());
+        println!("hcl: {}", self.hcl_okay());
+        println!("ecl: {}", self.ecl_okay());
+        println!("pid: {}", self.pid_okay());
+        println!();
+
+        if self.byr_okay()
+            && self.iyr_okay()
+            && self.eyr_okay()
+            && self.hgt_okay()
+            && self.hcl_okay()
+            && self.ecl_okay()
+            && self.pid_okay()
+        {
+            return true;
+        }
+        false
+    }
+
+    fn byr_okay(&self) -> bool {
+        if let Some(byr) = self.field.get("byr") {
+            let byr: u32 = match byr.parse() {
+                Ok(byr) => byr,
+                Err(_) => 0,
+            };
+
+            if byr >= 1920 && byr <= 2002 {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn iyr_okay(&self) -> bool {
+        if let Some(iyr) = self.field.get("iyr") {
+            let iyr: u32 = match iyr.parse() {
+                Ok(iyr) => iyr,
+                Err(_) => 0,
+            };
+
+            if iyr >= 1920 && iyr <= 2002 {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn eyr_okay(&self) -> bool {
+        if let Some(eyr) = self.field.get("eyr") {
+            let eyr: u32 = match eyr.parse() {
+                Ok(eyr) => eyr,
+                Err(_) => 0,
+            };
+            if eyr >= 2020 && eyr <= 2030 {
+                return true;
+            }
+        }
+
+        false
+    }
+
+    fn hgt_okay(&self) -> bool {
+        if let Some(hgt) = self.field.get("hgt") {
+            if hgt.ends_with("cm") {
+                let hgt: u32 = match hgt[..3].parse() {
+                    Ok(hgt) => hgt,
+                    Err(_) => 0,
+                };
+
+                if hgt >= 150 && hgt <= 190 {
+                    return true;
+                }
+            }
+
+            if hgt.ends_with("in") {
+                let hgt: u32 = match hgt[..2].parse() {
+                    Ok(hgt) => hgt,
+                    Err(_) => 0,
+                };
+
+                if hgt >= 59 && hgt <= 76 {
+                    return true;
+                }
+            }
+        }
+
+        false
+    }
+
+    fn hcl_okay(&self) -> bool {
+        if let Some(hcl) = self.field.get("hcl") {
+            return match i64::from_str_radix(&hcl[1..], 16) {
+                Ok(_) => true,
+                Err(_) => false,
+            };
+        }
+
+        false
+    }
+
+    fn ecl_okay(&self) -> bool {
+        if let Some(ecl) = self.field.get("ecl") {
+            return "amb blu brn gry grn hzl oth".contains(ecl);
+        }
+        false
+    }
+
+    fn pid_okay(&self) -> bool {
+        if let Some(pid) = self.field.get("pid") {
+            if pid.len() == 9 {
+                return pid.chars().all(char::is_numeric);
+            }
+        }
+        false
+    }
 }
