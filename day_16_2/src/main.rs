ea6446493887e0e170581e9d1fa27ab96b8ca085
James van de Ven <james@jamesvandeven.com>
2020-12-23 20:44:27 +0900

    small improvement on day_16_2

--- day_16_2/src/main.rs
+++ day_16_2/src/main.rs
@@ -15,15 +15,14 @@ fn main() {
             break;
         }
 
-        let ticket = line.split(": ");
+        let mut ticket = line.split(": ");
         let ticket_field = ticket.next().unwrap();
         let field_range: Vec<u32> = ticket
             .next()
             .unwrap()
             .split(" or ")
-            .map(|s: &str| s.split("-"))
-            .map(|num| (num).parse::<u32>().unwrap())
-            .collect::<Vec<u32>>();
+            .flat_map(|s: &str| s.split("-").map(|num| (num).parse::<u32>().unwrap()))
+            .collect();
 
         rulebook.push(Rule::new(
             ticket_field,
@@ -37,13 +36,24 @@ fn main() {
 
     let mut input_file = input_file.skip(4);
 
-    while let Some(line) = input_file.next() {}
+    while let Some(line) = input_file.next() {
+        ticket_vec.push(line.split(",").map(|s| s.parse::<u32>().unwrap()).collect());
+    }
+
+    // For each rule
+    for rule in rulebook {}
+    // Check any still unfound categories
+    // As soon as there is one failure
+    // Check another category
+    // Else if all tickets are okay
+    // Set rule value
 }
 
 struct Rule<'a> {
     ticket_field: &'a str,
     range_1: Range,
     range_2: Range,
+    field_num: Option<u32>,
 }
 
 impl<'a> Rule<'a> {
@@ -52,6 +62,7 @@ impl<'a> Rule<'a> {
             ticket_field,
             range_1: (min_1, max_1),
             range_2: (min_2, max_2),
+            field_num: None,
         }
     }
 }
